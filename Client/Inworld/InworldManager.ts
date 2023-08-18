ec5c9f5ab3106eaed7c6cd2b4c378bdb09d303f3
Bloc <92448509+BlocTheWorker@users.noreply.github.com>
2023-08-18 18:32:34 +0100

    Add release version files

--- Client/Inworld/InworldManager.ts
+++ Client/Inworld/InworldManager.ts
@@ -1,8 +1,8 @@
 // @ts-check
-import {
-    InworldClient
-} from '@inworld/nodejs-sdk';
-import InworldWorkspaceManager from './InworldWorkspaceManager';
+import {InworldClient, InworldConnectionService} from '@inworld/nodejs-sdk';
+import InworldWorkspaceManager from './InworldWorkspaceManager.js';
+import {BLCRecorder} from './Audio/BLCRecorder.js';
+import {SkyrimInworldSocketController,GetSocketResponse} from './SkyrimInworldSocketController.js';
 
 const WORKSPACE_NAME = process.env.INWORLD_WORKSPACE;
 
@@ -12,69 +12,104 @@ const defaultConfigurationConnection = {
 }
 
 export default class InworldClientManager {
-    private connection : any;
-    private client: InworldClient;
-    private IsConnected: boolean;
-    private workspaceManager: InworldWorkspaceManager;
-
+    private connection : InworldConnectionService;
+    private client : InworldClient;
+    private IsConnected : boolean;
+    private workspaceManager : InworldWorkspaceManager;
+    private blcRecorder : BLCRecorder;
+    private socketController : SkyrimInworldSocketController;
+    private isVoiceConnected = false;
+    private isAudioSessionStarted = false;
     currentCapabilities = {
         audio: true,
-        emotions: true
+        emotions: true,
+        phonemes: true
     }
 
-    constructor() {
-        // Somewhere here create hand crafted characters from list
+    constructor() { 
         this.SetupWorkspaceAndClient();
     }
 
-    async SetupWorkspaceAndClient(){
+    async SetupWorkspaceAndClient() {
         this.workspaceManager = new InworldWorkspaceManager();
         this.CreateClient();
     }
 
-    ConnectToCharacter(characterId:string, playerName:string, callback:any) {
-        let id = this.workspaceManager.GetCharacterIdentifier(characterId);
-        if(!id) {
-            console.log(`Cannot connect to ${id}`);
-            return;
+    // Socket version of connection
+    async ConnectToCharacterViaSocket(characterId : string, playerName : string, socket : WebSocket) {
+        try {
+            let id = this.workspaceManager.GetCharacterIdentifier(characterId);
+            console.log(`Requested to talk with ${characterId} which corresponds to ${id} on database.`);
+            (console as any).logToLog(`Requested to talk with ${characterId} which corresponds to ${id} on database.`)
+            if (! id) {
+                let errorResult = `Cannot connect to ${id} or ${characterId}`;
+                throw errorResult
+            }
+            console.log("Requesting connecting to " + id);
+            let scene = "workspaces/" + WORKSPACE_NAME + "/characters/{CHARACTER_NAME}".replace("{CHARACTER_NAME}", id);
+            this.client.setUser({fullName: playerName});
+            this.client.setScene(scene);
+
+            this.socketController = new SkyrimInworldSocketController(socket);
+            this.client.setOnMessage((dat : any) => this.socketController.ProcessMessage(dat));
+
+            this.client.setOnError((err) => {
+                if (err.code != 10 && err.code != 1) 
+                    console.error(err);
+            });
+            this.connection = this.client.build();
+            this.IsConnected = true;
+            if (!this.isVoiceConnected) {
+                console.log("Creating voice listener connection");
+                let port = parseInt(process.env.AUDIO_PORT);
+                this.blcRecorder = new BLCRecorder("127.0.0.1", port);
+                this.blcRecorder.connect(this.connection);
+            }
+            this.socketController.SetRecorder(this.blcRecorder);
+
+            let verifyConnection = GetSocketResponse("connection established", "1-1", "established");
+            console.log("Connection to " + id + " is succesfull");
+            (console as any).logToLog(`Connection to ${id} is succesfull.`)
+            await this.connection.sendAudioSessionStart();
+            this.isAudioSessionStarted = true;
+            socket.send(JSON.stringify(verifyConnection));
+        } catch(err) {
+            console.error(err);
+            let returnDoesNotExist = GetSocketResponse("This soul lacks the divine blessing of conversational endowment bestowed by the gods.", "1-1", "doesntexist");
+            socket.send(JSON.stringify(returnDoesNotExist));
         }
-        console.log("Requesting connecting to " + id);
-        let scene = "workspaces/" + WORKSPACE_NAME + "/characters/{CHARACTER_NAME}".replace("{CHARACTER_NAME}", id);
-        this.client.setUser({
-            fullName: playerName
-        })
-        this.client.setScene(scene);
-        this.client.setOnMessage(callback);
-        this.client.setOnError((err) => {
-            if (err.code != 10 && err.code != 1)
-                console.error(err);
-        });
-        this.connection = this.client.build();
-        this.IsConnected = true;
-        return this.connection;
     }
 
-    Say(message: string){
-        if(this.IsConnected) {
+    Say(message : string) {
+        if (this.IsConnected) {
             this.connection.sendText(message);
         }
     }
 
+    async StartTalking() {
+        if (!this.isAudioSessionStarted) {
+            await this.connection.sendAudioSessionStart();
+            this.isAudioSessionStarted = true;
+        }
+        await this.blcRecorder.start();
+    }
+
+    async StopTalking() {
+        setTimeout(async () => {
+            await this.blcRecorder.stop();
+            await this.connection.sendAudioSessionEnd();
+            this.isAudioSessionStarted = false;
+            this.socketController.SendUserVoiceInput();
+        }, 500)
+    }
+
     CreateClient() {
         this.client = new InworldClient();
-        this.client.setApiKey({
-            key: process.env.INWORLD_KEY as string,
-            secret: process.env.INWORLD_SECRET as string,
-        });
+        this.client.setApiKey({key: process.env.INWORLD_KEY as string, secret: process.env.INWORLD_SECRET as string});
         this.SetConfiguration();
     }
 
     SetConfiguration() {
-        this.client.setConfiguration({
-            connection: defaultConfigurationConnection,
-            capabilities: this.currentCapabilities
-        });
+        this.client.setConfiguration({connection: defaultConfigurationConnection, capabilities: this.currentCapabilities});
     }
-
-
 }
--- Client/SkyrimClient.ts
+++ Client/SkyrimClient.ts
@@ -1,82 +1,146 @@
-// @ts-check
 import * as dotenv from 'dotenv'
-import websocketPlugin from "@fastify/websocket"
-import Fastify from 'fastify'
+import * as fs from 'fs';
+import websocketPlugin, {SocketStream} from "@fastify/websocket"
+import Fastify, {FastifyRequest} from 'fastify'
+import InworldClientManager from "./Inworld/InworldManager.js";
+import path from "path";
+
+const resolved = path.resolve(".env");
+console.log("Reading .env from location: ", resolved);
 try {
-  dotenv.config()
+    dotenv.config({path: resolved})
 } catch (e) {
-  console.error("Something is not right with your env config!", e)
+    console.error("Something is not right with your env config!", e)
 }
-const fastify = Fastify({
-  logger: true
-});
+
+const fastify = Fastify({logger: true});
 fastify.register(websocketPlugin);
-import InworldClientManager from "./Inworld/InworldManager";
 
 const ClientManager = new InworldClientManager();
-var Connection: any;
-let CombinedResponse: string;
-let IsInteractionEnded:boolean;
-let LastConnectedUser: string;
 
-function SocketObserver(msg: any) {
-  if (msg.type == 'AUDIO') {
-    // dont use for now
-  } else if (msg.emotions) {
-    // dont use for now
-  } else if (msg.isText()) {
-    let responseMessage = msg.text.text;
-    CombinedResponse += responseMessage;
-  } else if (msg.isInteractionEnd()) {
-    IsInteractionEnded = true;
-  }
-}
 
-fastify.get('/ping', (request, reply) => {
-  return { 'status': "OK"}
+process.on('uncaughtException', function  (err, origin) {
+    console.error('Caught exception: ', err, origin);
+    logToLog(JSON.stringify({err, origin}));
 });
 
-// Say
-fastify.post('/say', async (request, reply) => {
-  let requestBody = request.body as any;
-  if (Connection == null) {
-    console.log("Connection is empty. Creating a new connection")
-    Connection = ClientManager.ConnectToCharacter(requestBody.id, "Dragonborn", SocketObserver);
-    LastConnectedUser = requestBody.id;
-  } else {
-    if(LastConnectedUser != requestBody.id) {
-      console.log(`Connection is for ${LastConnectedUser} and we requested it for ${requestBody.id} so we are creating new one`)
-      LastConnectedUser = requestBody.id;
-      Connection = ClientManager.ConnectToCharacter(requestBody.id, "Dragonborn", SocketObserver);
-    }
-  }
-  
-  if(requestBody.message == "") return "What?";
-  CombinedResponse = "";
-  IsInteractionEnded = false;
-  ClientManager.Say(requestBody.message);
-  return await new Promise( (resolve, reject) => {
-    let interval = setInterval(() => {
-      if (IsInteractionEnded) {
-        // Clear the interval and resolve the promise with the combined response
-        clearInterval(interval);
-        resolve({ "message": CombinedResponse});
-      }
-    }, 100);
-  });
+process.on('unhandledRejection', function (err, origin) {
+    console.error('Caught rejection: ', err, origin);
+    logToLog(JSON.stringify({err, origin}));
+});
+
+RunInformation();
+OverrideConsole();
+logToLog("=============================S=T=A=R=T=I=N=G===T=H=E===M=O=D=============================");
+
+fastify.get('/ping', (request, reply) => {
+    return {'status': "OK"}
+});
+
+// Socket connection for better communication channel
+fastify.register(async function (fastify) {
+    fastify.get('/chat', {
+        websocket: true
+    }, (connection : SocketStream, req : FastifyRequest) => {
+        connection.socket.on('message', msg => {
+            let message = JSON.parse(msg.toString());
+            console.log("Message received", message);
+            if (message.type == "connect") {
+                ClientManager.ConnectToCharacterViaSocket(message.id, "Dragonborn", connection.socket);
+            } else if (message.type == "start_listen") {
+                ClientManager.StartTalking();
+            } else if (message.type == "stop_listen") {
+                ClientManager.StopTalking();
+            } else if (message.type == "message") {
+                ClientManager.Say(message.message);
+            }
+        })
+    })
 });
 
 // Run the server!
 const StartEngine = async () => {
-  try {
-    await fastify.listen({
-      port: 3000
-    })
-  } catch (err) {
-    fastify.log.error(err);
-    console.error(err);
-    process.exit(1)
-  }
-}
+    try {
+        let portOnConfig = parseInt(process.env.CLIENT_PORT);
+        await fastify.listen({port: portOnConfig})
+    } catch (err) {
+        logToLog(JSON.stringify(err));
+        fastify.log.error(err);
+        console.error(err);
+        process.exit(1)
+    }
+}; 
 
 StartEngine();
+
+
+function RunInformation(){
+    console.log("\x1b[32m",`                                                                                                    
+                                                                                                    
+                                                                                                    
+                                             @        ,                                             
+                                            @@@       @@#                                           
+                                           @@           @@                                          
+                                          @@    (       &@@                                         
+                                         @@@   &@@@@     @@@                                        
+                                       *@@@    @@  (@%    @&@                                       
+                                      @@,@*   (      #@   @@ @                                      
+                                     @@ @@@        @@,    @@@ @,                                    
+                                    @@ @@      @@,          @@ @@                                   
+                                   @@ @@&      @@@          @@@ @@                                  
+                                  @@.@@@    @#@ @@@*  .@&   %@@@.@@                                 
+                                  @@ @@@&&, &@@@@@@@@@ @   &@@@ @@/                                 
+                                   @@#@@@ @@@@@@@@ @@@@@@@@@@@,@@                                   
+                                    (@@@@@@@% @@@@@@@@# /@@@@&@@                                    
+                                      @@@@@  @  @@@.@ &   #@@@@                                     
+                                       @@@,       @@.     @@@@                                      
+                                        @@@       @@@     @@@                                       
+                                         @@@@@    &@@  @@@@&                                        
+                                          @@@@   @@    @@@                                          
+                                           @@@  @&     &@                                           
+                                            ,@  %@                                                  
+                                                &@                                                  
+                                               @                                                    
+                                                @, #@                                               
+                                                 @@&                                                
+                                                  ,                                                 
+                                                                                                   `);
+    console.log("\x1b[34m", "****************************************************");
+    console.log("\x1b[32m", "Don't worry, you are suppose to see this!");
+    console.log("\x1b[34m", "\n****************************************************\n\n");
+    console.log("\x1b[31m", "DONT close this window or other window that opens if you want to use the mod. Close both only once you are done with the game. (Especially audio one because it really hits CPU)");
+    console.log("\x1b[32m", "Errors will shown here.");
+}
+
+function OverrideConsole() {
+    const originalLog = console.log;
+    console.log = function () {
+        const timestamp: string = new Date().toISOString();
+        const args = Array.prototype.slice.call(arguments);
+        args.unshift(`[${timestamp}]`);
+        originalLog.apply(console, args);
+    };
+    const originalError = console.error;
+    console.error = function () {
+        const timestamp: string = new Date().toISOString();
+        const args = Array.prototype.slice.call(arguments);
+        args.unshift(`[${timestamp}]`);
+        originalError.apply(console, args);
+        logToLog(JSON.stringify(args));
+    };
+
+    (console as any).logToLog =   function () {
+        logToLog(JSON.stringify(Array.prototype.slice.call(arguments)));
+    }
+}
+
+function logToLog(message: string): void {
+    const timestamp: string = new Date().toISOString();
+    const logMessage: string = `${timestamp} - ${message}`;
+    const logFileName = "InworldClient.log"
+    if (fs.existsSync(logFileName)) { // File exists, append to it
+        fs.appendFileSync(logFileName, logMessage + '\n', 'utf8');
+    } else { // File does not exist, create it and write the log message
+        fs.writeFileSync(logFileName, logMessage + '\n', 'utf8');
+    }
+}
